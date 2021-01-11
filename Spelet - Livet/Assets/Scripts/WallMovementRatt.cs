2e4e23ace5380e8c92a2cc27348c918008849e98
nealandin <75014196+nealandin@users.noreply.github.com>
2021-01-11 17:26:12 +0100

    push

---
+++ Spelet - Livet/Assets/Scripts/WallMovementRatt.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class WallMovementRatt : MonoBehaviour
+
+{
+
+ // Player Movement Variables/....
+ public static int movespeed = 1;
+ public Vector3 userDirection = Vector3.right;
+
+ public void FixedUpdate()
+ {
+     transform.Translate(userDirection * movespeed * Time.deltaTime); 
+ }
+}
