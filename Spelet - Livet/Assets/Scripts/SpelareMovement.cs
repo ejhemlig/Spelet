d45851398a72ba52739e2678e1f26b8938df0246
nealandin <75014196+nealandin@users.noreply.github.com>
2021-01-11 17:00:54 +0100

    updates

--- Spelet - Livet/Assets/Scripts/SpelareMovement.cs
+++ Spelet - Livet/Assets/Scripts/SpelareMovement.cs
@@ -1,18 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class SpelareMovement : MonoBehaviour
+public class MyPlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    float PlayerSpeed = 4f;
+    [SerializeField] 
+    float PlayerRotationSpeed = 20f;
+    private Transform myPlayer;
+    private CharacterController myControllerPlayer;
+    public AudioSource source; 
+    void Awake()
     {
-        
+        myPlayer = GetComponent<Transform>();
+        myControllerPlayer = GetComponent<CharacterController>();
     }
-
     // Update is called once per frame
     void Update()
     {
-        
+        // rotera
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        myPlayer.Rotate(
+                        0f,
+                        PlayerRotationSpeed * Time.deltaTime * moveHorizontal,
+                        0f
+                        );
+        // flytta    
+        float moveVertical = Input.GetAxis("Vertical");
+        myControllerPlayer.SimpleMove(myPlayer.forward * PlayerSpeed * moveVertical);
     }
 }
